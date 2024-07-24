a025e4d4c62a63946c594763a36fd06856e066f2
Thierrymanuel11 <thierrymanuel8@gmail.com>
2024-07-24 14:23:36 -0400

    Light update

---
+++ core_java/jdbc/src/test/java/JsonParser.java
@@ -0,0 +1,13 @@
+public class JsonParser {
+    /**
+     * Convert a java object to JSON String
+     * @param object input object
+     * @return JSON String
+     * @throws JsonProcessingException
+     *
+     *
+     */
+    public static String toJson(Object object, boolean prettyJson, boolean includeNullValues) throws JsonProcessingException{
+
+    }
+}
